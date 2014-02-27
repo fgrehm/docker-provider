f5f5e65d9154ae89a27bb4677f34e0da50a6f2f3
Fabio Rehm <fgrehm@gmail.com>
2014-02-27 00:47:51 -0300

    Do not hardcode path to base box used on acceptance specs

--- vagrant-spec.config.rb
+++ vagrant-spec.config.rb
@@ -2,7 +2,7 @@ Vagrant::Spec::Acceptance.configure do |c|
   c.component_paths << File.expand_path("../spec/acceptance", __FILE__)
 
   c.provider "docker",
-    # TODO: CHANGE TO A BOX URL
-    box: "/home/projects/oss/docker-provider/boxes/precise/precise.box",
+    # wget http://bit.ly/vagrant-docker-precise -O precise.box
+    box: "#{File.expand_path("../", __FILE__)}/precise.box",
     features: ['!suspend']
 end
